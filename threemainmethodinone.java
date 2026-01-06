8f162d69741a2ee91ed0953aaceecc76c5146957
Shreya Rodge <shreyarodge1006@gmail.com>
2026-01-06 21:32:17 +0530

    3 main method in single program

---
+++ threemainmethodinone.java
@@ -0,0 +1,15 @@
+ class threemainmethodinone {
+    public static void main(String[] args) {
+        System.out.println("This is the main method of threemainmethodinone class.");
+    }
+}
+class b{
+    public static void main(String[] args) {
+        System.out.println("This is the main method of class b.");
+    }
+}
+class c{
+    public static void main(String[] args) {
+        System.out.println("This is the main method of class C.");
+    }
+}
